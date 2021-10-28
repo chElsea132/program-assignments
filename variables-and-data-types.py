7fe97eb24e084c4bb1aace42bc1941f5c1e5bbe9
chElsea132 <92410609+chElsea132@users.noreply.github.com>
2021-10-28 19:41:19 -0400

    python practice

---
+++ variables-and-data-types.py
@@ -0,0 +1,52 @@
+print("Hello World")
+my_text="Hello World"
+print(my_text)
+glass_of_water=3
+print("I drank", 3 , "glasses of water today.")
+glass_of_water=4
+glass_of_water=glass_of_water +1
+print("glass_of_water")
+men_stepped_on_the_moon=4
+print(men_stepped_on_the_moon)
+my_reason_for_coding=7
+print(my_reason_for_coding)
+global_mean_sea_level_2018=21
+global_mean_sea_level_2018=0.21
+print(global_mean_sea_level_2018)
+staying_alive=True
+print(staying_alive)
+men_stepped_on_the_moon=4
+answer_1=type(men_stepped_on_the_moon)
+print(answer_1)
+my_reason_for_coding=7
+answer_2=7
+print(answer_2)
+answer_1=men_stepped_on_the_moon
+my_grade="11"
+answer_5=my_grade
+print(answer_5)
+my_temp=97.70
+answer_6=97
+print(answer_6)
+gift_list=[]
+answer_9=type(gift_list)
+print(answer_9)
+gift_list=[1,2,3,4]
+print(gift_list[0])
+print(gift_list[3])
+name={'Chelsea':{'Last name':'Brooks'},'John':'Smith'}
+print(name["Chelsea"])
+print(name["Chelsea"]['Last name'])
+my_tuple=()
+print(my_tuple)
+my_tuple= (1,2,3)
+print(my_tuple) 
+bucket_list=()
+answer_11=my_tuple
+print(answer_11)
+gift_list=[2,3,4,5]
+print(gift_list)
+colors={'Red','Ham','Purple','Yellow','Blue'}
+print(colors)
+my_tuple=(1,2,3,4,5)
+print(my_tuple[2])
