f3348d6c6c313a06f4aa29a23d2158aef1fb37d7
chElsea132 <92410609+chElsea132@users.noreply.github.com>
2021-11-02 19:09:30 -0400

    adding dictionary_practice

---
+++ dictionary_practice.py
@@ -0,0 +1,19 @@
+nba_players = ["Lebron James", "Kevin Durant", "Stephen Curry", "Damian Lillard", "Kawhi Leonard"]
+print("\nIndex of Lebron James:")
+print(nba_players[0])
+food_items= {"Chicken": 1.59,"Beef":1.99, "Cheese":1.00, "Milk": 2.50 }
+print(food_items["Milk"])
+age_numbers= {"Jaden": 3, "Matthew": 12, "Tyson": 16, "Chelsea": 22}
+print(age_numbers)
+print(age_numbers["Jaden"])
+shoes_numbers= {"Jordan 13": 1,"Yeezy": 8,"Foamposite": 10,"Air Max": 5,"SB Dunk": 20}
+print(shoes_numbers["Yeezy"])
+shoes_numbers["Jordan 13"] = 8
+shoes_numbers= {"Jordan 13": 8, "Yeezy": 16, "Foamposite": 18,"Air Max": 13,"SB Dunk": 28}
+print(shoes_numbers["SB Dunk"])
+shoes_numbers= {"Jordan 13": 5, "Yeezy": 11, "Foamposite": 15,"Air Max": 10,"SB Dunk": 25}
+print(shoes_numbers["Yeezy"])
+shoes_numbers= {"Jordan 13": 5, "Yeezy": 11, "Foamposite": 15,"Air Max": 10,"SB Dunk": 25, "Converse": 25, "Nike": 37}
+del shoes_numbers["Yeezy"]
+print(shoes_numbers["Air Max"]
+
